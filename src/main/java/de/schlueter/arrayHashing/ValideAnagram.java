02f73c433b6539da7aaafd5d95d79527fdf4f496
Maduki-tech <d.schlueter1011@gmail.com>
2024-02-21 15:47:18 +0100

    added faster solution with sorting

--- src/main/java/de/schlueter/arrayHashing/ValideAnagram.java
+++ src/main/java/de/schlueter/arrayHashing/ValideAnagram.java
@@ -1,25 +1,40 @@
 package de.schlueter.arrayHashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
  * ValideAnagram
  */
 public class ValideAnagram {
+    // public boolean isAnagram(String s, String t) {
+    //     HashMap<String, Integer> map = new HashMap<>();
+    //     for (String letter : s.split("")) {
+    //         int amount = map.get(letter) != null ? map.get(letter) : 0;
+    //         map.put(letter, amount + 1);
+    //     }
+    //
+    //     for (String letter : t.split("")) {
+    //         int amount = map.get(letter) != null ? map.get(letter) : 0;
+    //         map.put(letter, amount - 1);
+    //     }
+    //
+    //     return !map.values().stream().anyMatch(value -> value != 0);
+    // }
+    //
+
     public boolean isAnagram(String s, String t) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String letter : s.split("")) {
-            int amount = map.get(letter) != null ? map.get(letter) : 0;
-            map.put(letter, amount + 1);
+        char[] sToChar = s.toCharArray();
+        char[] tToChar = t.toCharArray();
+
+
+        Arrays.sort(sToChar);
+        Arrays.sort(tToChar);
+
+        if(s.length() != t.length()){
+            return false;
         }
 
-        for (String letter : t.split("")) {
-            int amount = map.get(letter) != null ? map.get(letter) : 0;
-            map.put(letter, amount - 1);
-        }
-
-        return !map.values().stream().anyMatch(value -> value != 0);
-
-
+        return Arrays.equals(sToChar, tToChar);
     }
 }
