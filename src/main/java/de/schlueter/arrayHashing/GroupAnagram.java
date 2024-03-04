341bd32ebb93f9bb90d4a31426b299a9ae44262a
Maduki-tech <d.schlueter1011@gmail.com>
2024-03-04 18:15:35 +0100

    added explaination

--- src/main/java/de/schlueter/arrayHashing/GroupAnagram.java
+++ src/main/java/de/schlueter/arrayHashing/GroupAnagram.java
@@ -8,20 +8,29 @@ import java.util.List;
 /**
  * GroupAnagram
  */
+
+/**
+* So the Map looks like this:
+* "aet" : ["eat", "tea", "ate"]
+*/
 public class GroupAnagram {
     public List<List<String>> groupAnagra(String[] strs) {
         List<List<String>> result = new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
 
         for (String string : strs) {
+            // Split the string into a char array and sort it
             char[] stringToChar = string.toCharArray();
             Arrays.sort(stringToChar);
             String sortedString = new String(stringToChar);
 
+            
+            // If the sorted string is not in the map add it
             if (!map.containsKey(sortedString)) {
                 map.put(sortedString, new ArrayList<>());
             }
 
+            // Add the string to the map
             map.get(sortedString).add(string);
         }
 
